c5fb6fcbf87e4a9f68ba328fd09bbf4b35d3a0e2
Deni Gunawan <artdengun@gmail.com>
2020-11-04 21:16:10 +0700

    sistem informasi pengolahan aset

---
+++ src/main/java/com/kpaydeveloper/sisfoaset/SistemInformasiAsetApplication.java
@@ -0,0 +1,13 @@
+package com.kpaydeveloper.sisfoaset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiAsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiAsetApplication.class, args);
+	}
+
+}
